fb44e12595ddc3d0f3882c59bef1787d0ff89a77
nedrett <nedret88@gmail.com>
2022-12-04 15:51:06 +0200

    Added Statistics in Home Page

---
+++ HouseRentingSystem.WebApi/Controllers/StatisticsApiController.cs
@@ -0,0 +1,33 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models.Statistics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.WebApi.Controllers
+{
+    [Route("api/statistics")]
+    [ApiController]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly IStatisticsService service;
+
+        public StatisticsApiController(IStatisticsService _service)
+        {
+            service = _service;
+        }
+
+        /// <summary>
+        /// Gets statistics about number of Houses and Rented Houses
+        /// </summary>
+        /// <returns>Total houses and total rents</returns>
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200, Type = typeof(StatisticsServiceModel))]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetStatistics()
+        {
+            var model = await service.Total();
+
+            return Ok(model);
+        }
+    }
+}
---
+++ HouseRentingSystem.WebApi/Extensions/HouseRentingApiServiceCollectionExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using HouseRentingSystem.Core.Contracts;
+    using HouseRentingSystem.Core.Exceptions;
+    using HouseRentingSystem.Core.Services;
+    using HouseRentingSystem.Infrastructure.Data;
+    using HouseRentingSystem.Infrastructure.Data.Common;
+    using Microsoft.EntityFrameworkCore;
+
+    public static class HouseRentingApiServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IStatisticsService, StatisticsService>();
+            services.AddScoped<IGuard, Guard>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddHouseRentingDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+    }
+}
